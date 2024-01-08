749e66c87d1c341a5f3574ad1511fc1e34d08a0e
JongHyuckLee <yarn add --dev --exact prettier>
2024-01-09 01:15:34 +0900

    스타벅스 세번째 페이지 완료

--- lib/starbucks.dart
+++ lib/starbucks.dart
@@ -610,9 +610,113 @@ class StarbucksThirdPage extends StatelessWidget {
   ];
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Starbucks 세 번째 페이지'),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            "Order",
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          backgroundColor: Colors.white,
+          actions: [
+            IconButton(
+              onPressed: () {
+                print("Pay 우측 상단 아이콘 클릭 됨");
+              },
+              icon: Icon(
+                Icons.search_outlined,
+                color: Colors.grey,
+              ),
+            ),
+          ],
+
+          /// Tip : AppBar 하단에 TabBar를 만들어 줍니다.
+          bottom: TabBar(
+            isScrollable: false,
+            indicatorColor: starbucksPrimaryColor,
+            indicatorWeight: 4,
+            labelColor: Colors.black,
+            unselectedLabelColor: Colors.grey,
+            labelStyle: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+            tabs: [
+              Tab(text: "전체 메뉴"),
+              Tab(text: "나만의 메뉴"),
+              Tab(text: "🎂 홀케이크"),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            //전체 메뉴
+            ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                final item = menu[index % menu.length];
+                final ko = item["ko"] ?? "제목";
+                final en = item["en"] ?? "Title";
+                final imgUrl = item["imgUrl"] ?? "";
+
+                return Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 21,
+                    vertical: 16,
+                  ),
+                  child: Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 52,
+                        // Tip : circleAvatar 배경에 맞춰서 동그랗게 이미지 넣는 방법
+                        backgroundImage: NetworkImage(imgUrl),
+                        backgroundColor: Colors.transparent,
+                      ),
+                      SizedBox(
+                        width: 16,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            ko,
+                            style: TextStyle(
+                              fontSize: 21,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 6,
+                          ),
+                          Text(
+                            en,
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.grey,
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+            // 나만의 메뉴
+            Center(
+              child: Text("나만의 메뉴"),
+            ),
+            // 홀케이크
+            Center(
+              child: Text("홀케이크 예약"),
+            ),
+          ],
+        ),
       ),
     );
   }
